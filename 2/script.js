c27d05b472612783ac4c2c422053558602b333ee
Tanya <Chagovskaya>
2016-09-24 15:47:03 +0300

    removed error

--- 2/script.js
+++ 2/script.js
@@ -1,11 +1,14 @@
-var names = ['Андрей', 'Татьяна', 'Константин', 'Александра', 'Артем'];
+var names = [];
+for (var i = 0; i < 5; i++) {
+    names[i] = prompt('Введите любое имя:', '');
+}
 var addName = prompt ("Введите свое имя", '');
 var message = 'Ошибка';
 
 for (var i = 0; i < names.length; i++) {
     if (names[i] === addName) {
-        message = addName + ', Вы успешно вошли';
+        var message = addName + ', Вы успешно вошли';
     }
 }
 
-alert(message);
+alert(message)
